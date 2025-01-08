d1f5cc11a6697ccec405dd203073c82950ce199b
park671 <youngpark_671@163.com>
2025-01-08 18:42:31 +0800

    start support .bss fill 0

--- main.c
+++ main.c
@@ -1,7 +1,17 @@
 #include "uart.h"
 
+//use for test .bss
+int x;
+int y;
+int z;
+int a;
+int b;
+int c;
+
 void main() {
     uart_printf("park hello world!\n\r");
-    uart_printf("park hello world!\n\r");
-    uart_printf("park hello world!\n\r");
+}
+
+void on_bss_fill() {
+    uart_printf(".bss fill 8byte\n\r");
 }
--- start.s
+++ start.s
@@ -1,6 +1,36 @@
     .global _start
 _start:
-    ldr x30, =stack_top
-    mov sp, x30
+    ldr x0, =stack_top
+    mov sp, x0
+    mov x29, #0
+    mov x30, #0
+
+    //enable fp & simd
+    mrs x0, cpacr_el1
+    orr x0, x0, #0x30000000
+    msr cpacr_el1, x0
+
+    //memset(bss, 0, sizeof(bss));
+    sub sp, sp, #24
+    ldr x0, =bss_start
+    ldr x1, =bss_end
+fill_loop_start:
+    cmp x0, x1
+    b.cs fill_loop_finish
+    mov x2, #0
+    str x2, [x0], #8
+    //save ctx
+    str x0, [sp, #16]
+    str x1, [sp, #8]
+    str x2, [sp]
+    bl on_bss_fill
+    //restore ctx
+    ldr x0, [sp, #16]
+    ldr x1, [sp, #8]
+    ldr x2, [sp]
+    b fill_loop_start
+fill_loop_finish:
+    add sp, sp, #24
+
     bl main
     b .
